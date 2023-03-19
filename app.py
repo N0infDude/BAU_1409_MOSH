9fb14c7748149c2d3a5a1593d84366f54f99c0b1
chetTest <alexu-che@yandex.ru>
2023-03-19 13:57:20 +0300

    did stuf

--- app.py
+++ app.py
@@ -12,16 +12,20 @@ app = Flask(__name__)
 @app.route("/index.html")
 @app.route("/")
 def index():
-    return "first page"
+    return render_template('index.html')
 
 @app.route("/secondpage")
-def index():
-    data = {'key': ''}
+def second():
+    data = {'token': ''}
     for key in data:
-        data[key] = request.args.get(key)
+        data[key] = request.args.get(key, "")
+    if debug == 1: print(data)
     url = 'https://dt.miet.ru/ppo_it_final'
     param = {'X-Auth-Token': 'v9khcqgk'}
     resp = requests.get(url=url, headers=param)
-
+    if debug == 1: print(resp.json())
 
     return "second page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
